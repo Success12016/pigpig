1816e6767a9add687945b6c8cc8f3eaa1729c961
Success12016 <pattarachai12016@gmail.com>
2023-03-12 14:27:04 +0700

    login and register

---
+++ login.php
@@ -0,0 +1,66 @@
+<?php
+    session_start();
+    if(isset($_SESSION['id'])){
+        header("location:index.php");
+        die();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container">
+    <h1 class="title has-text-centered">PigPig</h1>
+    <br>
+    <div class="columns">
+        <div class="column is-4"></div>
+        <div class="column is-4">
+            <?php
+            if(isset($_SESSION['error'])){
+                echo "<div class='alert alert-danger'>
+                ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง</div>";
+                unset($_SESSION['error']);
+            }
+            ?>
+            <div class="card">
+                <div class="has-text-centered">เข้าสู่ระบบ</div>
+                <div class="columns is-centered is-vcentered is-multiline is-mobile">
+                    <form action="verify.php" method="post">
+                        <div class="columns my-3 has-text-centered">
+                            <div class="column is-half">
+                                User ID
+                            </div>
+                            <div class="column is-half">
+                                <input type="text" name="login" class="form-control">
+                            </div>
+                        </div>
+                        <div class="columns my-3 has-text-centered">
+                            <div class="column is-half">
+                                Password
+                            </div>
+                            <div class="column is-half">
+                                <input type="password" name="pwd" class="form-control" id="password">
+                            </div>
+                        </div>
+                        <center>
+                            <div class="column">
+                                <button type="submit" class="button is-primary is-small">เข้าสู่ระบบ</button>
+                            </div>
+                        </center>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="column is-4"></div>
+    </div>
+    <br>
+    <p align= "center">ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัครสมาชิก</a></p>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,91 @@
+<?php
+    session_start();
+    if(isset($_SESSION['id'])){
+        header("location:index.php");
+        die();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <title>Register</title>
+</head>
+<body>
+    <div class="container">
+    <h1 class="title has-text-centered">PigPig</h1>
+    <br>
+    <div class="columns">
+        <div class="column is-3"></div>
+        <div class="column is-6">
+            <?php
+                if(isset($_SESSION['add_login'])){
+                    if($_SESSION['add_login']=='error'){
+                    echo "<div class='alert alert-danger'>ชื่อบัญชีซ้ำหรือฐานข้อมูลมีปัญหา</div>";
+                }else{
+                    echo "<div class='alert alert-success'>เพิ่มบัญชีเรียบร้อยแล้ว</div>";
+                }
+                unset($_SESSION['add_login']);
+            }
+            ?>
+            <DIV class="card">
+                <div class="has-text-centered">กรอกข้อมูล</div>
+                <div class="columns is-centered is-vcentered is-multiline is-mobile">
+                    <form action="register_save.php" method="post">
+                        <div class="my-3 columns is-fullwidth">
+                            <div class="column is-half">
+                                User ID
+                            </div>
+                            <div class="column is-half">
+                                <input class="is-expanded form-control" type="text" name="login" required>
+                            </div>
+                        </div>
+                        <div class="my-3 columns is-fullwidth">
+                            <div class="column is-half">
+                                Password
+                            </div>
+                            <div class="column is-half">
+                                <input class="is-expanded form-control" type="password" name="pwd" required>
+                            </div>
+                        </div>
+                        <div class="my-3 columns is-fullwidth">
+                            <div class="column is-half">
+                                Pig Name
+                            </div>
+                            <div class="column is-half">
+                                <input class="is-expanded form-control" type="text" name="pigname" required>
+                            </div>
+                        </div>
+                        <div class="my-3 columns is-fullwidth">
+                            <div class="column is-half">
+                                อีเมล:
+                            </div>
+                            <div class="column is-half">
+                                <input class="is-expanded form-control" type="email" name="email"  required>
+                            </div>
+                        </div>
+                        <div class="has-text-centered">
+                            <div class="column">
+                                <button class="button is-primary is-small" type="submit">
+                                    สมัครสมาชิก
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </DIV>
+        </div>
+        <div class="column is-3"></div>
+    </div>
+    </div>
+    <br>
+    <p align= "center"><a href="login.php">กลับสู่หน้าเข้าสู่ระบบ</a></p>
+</body>
+</html>
